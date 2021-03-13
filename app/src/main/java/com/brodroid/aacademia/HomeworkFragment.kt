b4b7c2386eca7b357ac898526b2fe9b5e6624faf
Andrii Vynokurov <vinokurov.andrei89@gmail.com>
2021-03-13 18:13:17 +0200

    added show home work presentation to fragment

--- app/src/main/java/com/brodroid/aacademia/HomeworkFragment.kt
+++ app/src/main/java/com/brodroid/aacademia/HomeworkFragment.kt
@@ -4,28 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.brodroid.aacademia.ui.FragmentPdfView
+import com.brodroid.aacademia.util.configAndShow
 
-class HomeworkFragment : Fragment() {
+class HomeworkFragment : Fragment(R.layout.homework_fragment) {
 
-    companion object {
-        fun newInstance() = HomeworkFragment()
-    }
-
+    private val presentationUrl: String by lazy { requireArguments().getString(HOME_URL, "") }
     private lateinit var viewModel: HomeworkViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.homework_fragment, container, false)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(HomeworkViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (presentationUrl.isNotEmpty()) {
+            val webView: WebView = view.findViewById(R.id.webView)
+            webView.configAndShow(presentationUrl)
+        } else {
+            view.findViewById<TextView>(R.id.presentationEmptyMessage).isVisible = true
+        }
+    }
+
+    companion object {
+        private const val HOME_URL = "url"
+
+        fun newInstance(url: String) = HomeworkFragment().apply {
+            arguments = Bundle().apply {
+                putString(HOME_URL, url)
+            }
+        }
     }
 
 }
