d65b42f4c48a5b0557da6eab7463e12ff3f45d81
Andrii Vynokurov <vinokurov.andrei89@gmail.com>
2021-03-14 00:12:42 +0200

    added show video

--- app/src/main/java/com/brodroid/aacademia/VideoFragment.kt
+++ app/src/main/java/com/brodroid/aacademia/VideoFragment.kt
@@ -1,25 +1,43 @@
 package com.brodroid.aacademia
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.webkit.WebView
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.brodroid.aacademia.util.configAndShow
 
-class VideoFragment : Fragment() {
+class VideoFragment : Fragment(R.layout.video_fragment) {
 
-    companion object {
-        fun newInstance() = VideoFragment()
-    }
+    private val videoUrl: String by lazy { requireArguments().getString(VIDEO_URL, "") }
 
     private lateinit var viewModel: VideoViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.video_fragment, container, false)
+    companion object {
+        private const val VIDEO_URL = "url"
+        fun newInstance(url: String) = VideoFragment().apply {
+            arguments = Bundle().apply {
+                putString(VIDEO_URL, url)
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(VideoViewModel::class.java)
+        viewModel.checkWhatSHowToUser(videoUrl)
+        viewModel.onReadyToShow.observe(viewLifecycleOwner) { showWebViewWithUrl(videoUrl) }
+        viewModel.onEmptyUrl.observe(viewLifecycleOwner) {
+            view.findViewById<TextView>(R.id.emptyMessage).isVisible = true
+        }
+    }
+
+
+    private fun showWebViewWithUrl(videoUrl: String) {
+        val webView: WebView? = view?.findViewById(R.id.webView)
+        webView?.configAndShow(getString(R.string.fragment_video_link, videoUrl))
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
