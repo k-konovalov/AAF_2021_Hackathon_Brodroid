0049f0498f3a4cf1eed18468d1ec77e4ca281e5f
Vladimir Tsihukov <tishukooov@gmail.com>
2021-03-13 14:10:30 +0300

    Correct login in fragment and activity

--- app/src/main/java/com/brodroid/aacademia/LessonsFragment.kt
+++ app/src/main/java/com/brodroid/aacademia/LessonsFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.brodroid.aacademia.adapter.AdapterLessons
 import com.brodroid.aacademia.adapter.OnClickDetailLesson
+import com.brodroid.aacademia.viewModel.ViewModelLessons
 
 class LessonsFragment : Fragment(R.layout.fragment_lesson) {
 
     private lateinit var recyclerView: RecyclerView;
     private val adapterLesson by lazy { AdapterLessons(onClick) }
+
+    private val mViewModelLessons : ViewModelLessons by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,17 +23,20 @@ class LessonsFragment : Fragment(R.layout.fragment_lesson) {
         recyclerView = view.findViewById<RecyclerView>(R.id.rec_view_lesson).apply {
             adapter = adapterLesson
         }
-        setAdapter()
+
+        mViewModelLessons.liveDataLessonList.observe(viewLifecycleOwner, {listLesson ->
+            Toast.makeText(context, "List size = ${listLesson.size}", Toast.LENGTH_SHORT).show()
+            setAdapter(listLesson)
+        })
     }
 
-    private fun setAdapter() {
-        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
-        adapterLesson.bindLesson(list)
+    private fun setAdapter(listLesson: List<Lesson>) {
+        adapterLesson.bindLesson(listLesson)
     }
 
     private val onClick = object : OnClickDetailLesson {
-        override fun moveDetailLesson(position : Int) {
-            Toast.makeText(context, "Lesson №$position", Toast.LENGTH_SHORT).show()
+        override fun moveDetailLesson(listLesson: Lesson) {
+            Toast.makeText(context, "Lesson ${listLesson.name}", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/brodroid/aacademia/MainActivity.kt
+++ app/src/main/java/com/brodroid/aacademia/MainActivity.kt
@@ -1,45 +1,12 @@
 package com.brodroid.aacademia
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var firebaseDatabase: FirebaseDatabase? = null
-        var databaseReference: DatabaseReference? = null
-
-        firebaseDatabase = FirebaseDatabase.getInstance("https://aacademy-c0452-default-rtdb.firebaseio.com/")
-        databaseReference = firebaseDatabase.getReference("lessons")
-
-        databaseReference.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                // this method is call to get the realtime
-                // updates in the data.
-                // this method is called when the data is
-                // changed in our Firebase console.
-                // below line is for getting the data from
-                // snapshot of our database.
-                val lessons: MutableList<Lesson> = mutableListOf()
-                snapshot.children.mapNotNullTo(lessons) {
-                    it.getValue(Lesson::class.java)
-                    // after getting the value we are setting
-                    // our value to our text view in below line.
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                // calling on cancelled method when we receive
-                // any error or we are not able to get the data.
-                Toast.makeText(this@MainActivity, "Fail to get data.", Toast.LENGTH_SHORT).show()
-            }
-        })
     }
 }
