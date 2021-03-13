45cc6e43f20b51a0fd3ae4b74ccd15ad4c387f54
Andrii Vynokurov <vinokurov.andrei89@gmail.com>
2021-03-13 12:45:02 +0200

    Added presentation fragment with webView

--- app/src/main/java/com/brodroid/aacademia/MainActivity.kt
+++ app/src/main/java/com/brodroid/aacademia/MainActivity.kt
@@ -2,10 +2,20 @@ package com.brodroid.aacademia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.brodroid.aacademia.ui.FragmentPdfView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+    
+    /*How to use presentation fragment
+    * start with url
+    *  val url = "https://docs.google.com/presentation/d/1LyB8p2C4ZowWQRIxchMPDbrpBrdDN8MgXeQGEJVScJk/edit#slide=id.ga4a15a5964_0_76"
+    *
+      FragmentPdfView.newInstance(url)
+    * */
 }
